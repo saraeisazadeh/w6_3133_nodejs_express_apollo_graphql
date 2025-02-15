9d795ff7a3bb4ad50d7a4206054eb3cbf2392158
Sara Eisazadeh <101432538@georgebrown.ca>
2025-02-15 14:01:21 -0500

    Completed Lab 05 - GraphQL with MongoDB integration

--- resolvers.js
+++ resolvers.js
@@ -1,1 +1,62 @@
-const Movie = require('./models/Movie');
+const mongoose = require('mongoose');
+
+// Define the Movie schema
+const movieSchema = new mongoose.Schema({
+  name: String,
+  director_name: String,
+  production_house: String,
+  release_date: String,
+  rating: Number,
+});
+
+// Create the Movie model
+const Movie = mongoose.model('Movie', movieSchema);
+
+const resolvers = {
+  Query: {
+    // Query to get all movies
+    movies: async () => await Movie.find(),
+
+    // Query to get a movie by ID
+    movie: async (parent, args) => await Movie.findById(args.id),
+  },
+  Mutation: {
+    // Mutation to insert a new movie
+    addMovie: async (parent, args) => {
+      const newMovie = new Movie({
+        name: args.name,
+        director_name: args.director_name,
+        production_house: args.production_house,
+        release_date: args.release_date,
+        rating: args.rating,
+      });
+      return await newMovie.save();
+    },
+
+    // Mutation to update an existing movie
+    updateMovie: async (parent, args) => {
+      const updatedMovie = await Movie.findByIdAndUpdate(
+        args.id,
+        {
+          $set: {
+            name: args.name,
+            director_name: args.director_name,
+            production_house: args.production_house,
+            release_date: args.release_date,
+            rating: args.rating,
+          },
+        },
+        { new: true } // Return the updated document
+      );
+      return updatedMovie;
+    },
+
+    // Mutation to delete a movie by ID
+    deleteMovie: async (parent, args) => {
+      const deletedMovie = await Movie.findByIdAndDelete(args.id);
+      return deletedMovie;
+    },
+  },
+};
+
+module.exports = resolvers;
--- schema.js
+++ schema.js
@@ -1,1 +1,40 @@
 const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Movie {
+    id: ID!
+    name: String!
+    director_name: String!
+    production_house: String!
+    release_date: String!
+    rating: Float!
+  }
+
+  type Query {
+    movies: [Movie!]! # Query to get all movies
+    movie(id: ID!): Movie # Query to get a movie by ID
+  }
+
+  type Mutation {
+    addMovie(
+      name: String!
+      director_name: String!
+      production_house: String!
+      release_date: String!
+      rating: Float!
+    ): Movie # Mutation to insert a new movie
+
+    updateMovie(
+      id: ID!
+      name: String
+      director_name: String
+      production_house: String
+      release_date: String
+      rating: Float
+    ): Movie # Mutation to update an existing movie
+
+    deleteMovie(id: ID!): Movie # Mutation to delete a movie by ID
+  }
+`;
+
+module.exports = typeDefs;
